1b0928aaec15c77286a426145fa5e6f512714da2
Wendal Chen <wendal1985@gmail.com>
2020-03-08 23:12:48 +0800

    fix: qemu的文件系统没有自动初始化

--- applications/main.c
+++ applications/main.c
@@ -1,16 +1,24 @@
 
 #include <rtthread.h>
 #include <rtdevice.h>
-#include "lua.h"
-#include "lauxlib.h"
+
 #include "luat_base.h"
+
+static void _main(void* param) {
+    rt_thread_mdelay(100); // 故意延后100ms
+    luat_main(NULL, NULL, NULL);
+    while (1)
+        rt_thread_delay(10000000);
+}
 
 int main(void)
 {
-    luat_main(NULL, NULL, NULL);
-    while (1) {
-        rt_thread_mdelay(2000000);
-    }
+    rt_thread_t t = rt_thread_create("luat", _main, RT_NULL, 6*1024, 15, 20);
+    rt_thread_startup(t);
+    //luat_main(NULL, NULL, NULL);
+    //while (1) {
+    //    rt_thread_mdelay(2000000);
+    //}
     return 0;
 }
 
