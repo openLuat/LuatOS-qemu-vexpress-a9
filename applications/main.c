d5738e7f072a84d0df31956315ff3b46160a0f0f
Wendal Chen <wendal1985@gmail.com>
2020-03-08 23:16:19 +0800

    update: qemu内存够多,给luat线程分配32k内存吧!

--- applications/main.c
+++ applications/main.c
@@ -13,7 +13,7 @@ static void _main(void* param) {
 
 int main(void)
 {
-    rt_thread_t t = rt_thread_create("luat", _main, RT_NULL, 6*1024, 15, 20);
+    rt_thread_t t = rt_thread_create("luat", _main, RT_NULL, 32*1024, 15, 20);
     rt_thread_startup(t);
     //luat_main(NULL, NULL, NULL);
     //while (1) {
